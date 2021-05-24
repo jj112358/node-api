88e6e9cd78b1790208bf3638125d9816cc1c49de
brojie <jj112358@163.com>
2021-05-24 15:13:32 +0800

    10-错误处理

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,32 @@
-const { createUser } = require('../service/user.service')
+const { createUser, getUerInfo } = require('../service/user.service')
 
 class UserController {
   async register(ctx, next) {
     // 1. 获取数据
     // console.log(ctx.request.body)
     const { user_name, password } = ctx.request.body
+
+    // 合法性
+    if (!user_name || !password) {
+      console.error('用户名或密码为空', ctx.request.body)
+      ctx.status = 400
+      ctx.body = {
+        code: '10001',
+        message: '用户名或密码为空',
+        result: '',
+      }
+      return
+    }
+    // 合理性
+    if (getUerInfo({ user_name })) {
+      ctx.status = 409
+      ctx.body = {
+        code: '10002',
+        message: '用户已经存在',
+        result: '',
+      }
+      return
+    }
     // 2. 操作数据库
     const res = await createUser(user_name, password)
     // console.log(res)
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -3,17 +3,27 @@ const User = require('../model/use.model')
 class UserService {
   async createUser(user_name, password) {
     // 插入数据
-    // User.create({
-    //   // 表的字段
-    //   user_name: user_name,
-    //   password: password
-    // })
-
     // await表达式: promise对象的值
     const res = await User.create({ user_name, password })
     // console.log(res)
 
     return res.dataValues
+  }
+
+  async getUerInfo({ id, user_name, password, is_admin }) {
+    const whereOpt = {}
+
+    id && Object.assign(whereOpt, { id })
+    user_name && Object.assign(whereOpt, { user_name })
+    password && Object.assign(whereOpt, { password })
+    is_admin && Object.assign(whereOpt, { is_admin })
+
+    const res = await User.findOne({
+      attributes: ['id', 'user_name', 'password', 'is_admin'],
+      where: whereOpt,
+    })
+
+    return res ? res.dataValues : null
   }
 }
 
