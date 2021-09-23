798c871b01b94b49ee0f9faf1e9c76ba8faf8a07
brojie <jj112358@163.com>
2021-09-23 10:39:47 +0800

    30-添加到购物车(下)

---
+++ src/model/cart.model.js
@@ -0,0 +1,35 @@
+// 1. 导入sequelize的连接
+const { DataTypes } = require('sequelize')
+const seq = require('../db/seq')
+
+// 2. 定义Cart模型
+const Cart = seq.define('zd_carts', {
+  goods_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: '商品的id',
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: '用户的id',
+  },
+  number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+    comment: '商品的数量',
+  },
+  selected: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+    comment: '是否选中',
+  },
+})
+
+// 3. 同步数据(建表)
+// Cart.sync({ force: true })
+
+// 4. 导出Cart模型
+module.exports = Cart
--- src/service/cart.service.js
+++ src/service/cart.service.js
@@ -1,11 +1,27 @@
+const { Op } = require('sequelize')
+const Cart = require('../model/cart.model')
+
 class CartService {
   async createOrUpdate(user_id, goods_id) {
-    return {
-      id: 1,
-      user_id: 13,
-      goods_id: 1,
-      number: 1,
-      selected: true,
+    // 根据user_id和goods_id同时查找, 有没有记录
+    let res = await Cart.findOne({
+      where: {
+        [Op.and]: {
+          user_id,
+          goods_id,
+        },
+      },
+    })
+
+    if (res) {
+      // 已经存在一条记录, 将number + 1
+      await res.increment('number')
+      return await res.reload()
+    } else {
+      return await Cart.create({
+        user_id,
+        goods_id,
+      })
     }
   }
 }
